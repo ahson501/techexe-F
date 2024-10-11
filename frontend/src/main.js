63705af051ecee231de27ec080800e55fbdc76f2
ahson501 <techexe.net@gmail.com>
2024-10-11 11:53:04 +0500

    11-10a-2024

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,8 +4,11 @@
 
 import './css/portfolio.css'
 import './css/blog.css'
+import './css/complaints.css'
+
 
 
 // Any JavaScript logic for your portfolio app
 console.log("Portfolio site loaded with Vite!");
 console.log("blog site loaded with Vite!");
+console.log("complaints site loaded with Vite!");
--- frontend/static/frontend/dist/assets/main-BWKmEOBC.js
+++
@@ -1,1 +0,0 @@
-/* empty css                  *//* empty css             */console.log("Portfolio site loaded with Vite!");console.log("blog site loaded with Vite!");
