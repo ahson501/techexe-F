668f99f8ff64c4435af64bd89751b8aafa71a526
ahson501 <techexe.net@gmail.com>
2024-10-29 11:12:33 +0500

    29-10-2024

--- home/admin.py
+++ home/admin.py
@@ -1,7 +1,2 @@
 from django.contrib import admin
-from .models import HomeContent
 
-@admin.register(HomeContent)
-class HomeContentAdmin(admin.ModelAdmin):
-    list_display = ('title', 'subtitle', 'description', 'image')
-    search_fields = ('title', 'subtitle', 'description')
--- home/models.py
+++ home/models.py
@@ -1,10 +1,2 @@
 from django.db import models
 
-class HomeContent(models.Model):
-    title = models.CharField(max_length=100)
-    subtitle = models.CharField(max_length=100, blank=True, null=True)
-    description = models.TextField()
-    image = models.ImageField(upload_to='home_images/', blank=True, null=True)
-
-    def __str__(self):
-        return self.title
